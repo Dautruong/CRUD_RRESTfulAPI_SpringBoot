94d8076472ff1c9efe8f5e5c42c62424dcfd6fa3
dau <dautruongptit@gmail.com>
2022-05-10 10:57:22 +0700

    ok"

--- src/main/java/com/test/info/UserController.java
+++ src/main/java/com/test/info/UserController.java
@@ -15,42 +15,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class userController {
-	 @Autowired
-	 private userService userService;
-	 @RequestMapping(value = { "/users"}, method = RequestMethod.GET) 
-	 public List<User> list() {
-	     return userService.listAll();
-	 }
-	 @PostMapping("/users")
-	 public void add(@RequestBody User user) {
-		 userService.save(user);
-	 }
-	
-	 @GetMapping("/users/{id}")
-	 public ResponseEntity<User> get(@PathVariable Integer id) {
-	     try {
-	         User user = userService.get(id);
-	         return new ResponseEntity<User>(user, HttpStatus.OK);
-	     } catch (NoSuchElementException e) {
-	         return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-	     }      
-	 }
-	 
-	 @PutMapping("/users/{id}")
-	 public ResponseEntity<?> update(@RequestBody User user, @PathVariable Integer id) {
-	     try {
-	         User existUser = userService.get(id);
-	         userService.save(user);
-	         return new ResponseEntity<>(HttpStatus.OK);
-	     } catch (NoSuchElementException e) {
-	         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	     }      
-	 }
-	 @DeleteMapping(value = "/users/{id}")
-	 public void delete(@PathVariable Integer id) {
-		 userService.delete(id);
-	 }
-	   
+    @Autowired
+    private userService userService;
+
+    @RequestMapping(value = {"/users"}, method = RequestMethod.GET)
+    public List<User> list() {
+        return userService.listAll();
+    }
+
+    @PostMapping("/users")
+    public void add(@RequestBody User user) {
+        userService.save(user);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> get(@PathVariable Integer id) {
+        try {
+            User user = userService.get(id);
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<?> update(@RequestBody User user, @PathVariable Integer id) {
+        try {
+            User existUser = userService.get(id);
+            userService.save(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(value = "/users/{id}")
+    public void delete(@PathVariable Integer id) {
+        userService.delete(id);
+    }
+
 }
